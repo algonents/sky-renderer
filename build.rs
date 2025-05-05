1c7adce6f198d610181451606e66b5d615f00860
ludinar <ludinar@skysoft-atm.com>
2025-05-05 14:13:30 +0200

    prog: adapt build.rs for windows

--- build.rs
+++ build.rs
@@ -8,25 +8,43 @@ fn main() {
     let target = env::var("TARGET").unwrap();
 
     let dst = cmake::Config::new("cpp")
+        .define("CMAKE_PREFIX_PATH", "D:/GitHub/vcpkg/installed/x64-windows")
         .build_target("glrenderer")
         .static_crt(true)
         .build();
 
     let lib_dir = dst.join("build");
 
-    println!("cargo:rustc-link-search=native={}", lib_dir.display());
-    println!("cargo:rustc-link-lib=static=glrenderer");
-    println!("cargo:rustc-link-lib=dylib=glfw");
-
+    
     // handle platform-specific configuration
     if target.contains("linux") {
+        
+        println!("cargo:rustc-link-search=native={}", lib_dir.display());
+        println!("cargo:rustc-link-lib=static=glrenderer");
+    
+        println!("cargo:rustc-link-lib=dylib=glfw");
         println!("cargo:rustc-link-lib=dylib=GL");
         println!("cargo:rustc-link-lib=dylib=stdc++");
+    
     } else if target.contains("apple") {
+        
+        println!("cargo:rustc-link-search=native={}", lib_dir.display());
+        println!("cargo:rustc-link-lib=static=glrenderer");
+    
         let homebrew_lib_location = "/opt/homebrew/lib";
         println!("cargo:rustc-link-search=native={}", homebrew_lib_location);
+        println!("cargo:rustc-link-lib=dylib=glfw");
+
         println!("cargo:rustc-link-lib=dylib=c++");
+
     } else if target.contains("windows") {
-        // todo
+        
+        println!("cargo:rustc-link-search=native={}", lib_dir.join("Debug").display());
+        println!("cargo:rustc-link-lib=static=glrenderer");
+    
+        
+        let vcpkg_lib_location = "D:/GitHub/vcpkg/installed/x64-windows/lib";
+        println!("cargo:rustc-link-search=native={}", vcpkg_lib_location);
+        println!("cargo:rustc-link-lib=dylib=glfw3dll");
     }
 }
