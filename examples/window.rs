ac53a14e9bfeded281437b8dd73477172d40dafd
bigludinski <anwar.ludin@algonents.com>
2025-07-03 22:15:29 +0200

    prog: window resizing callback

--- examples/window.rs
+++ examples/window.rs
@@ -3,7 +3,11 @@ extern crate sky_renderer;
 use sky_renderer::core::{App, Window};
 
 fn main() {
-    let window = Window::new("Hello Window", 800, 600);
+    let mut  window = Window::new("Hello Window", 800, 600);
+    window.set_on_resize(move |w, h| {
+        println!("window resized, width:{}, height: {}", w, h);
+    });
+    
     let app = App::new(window);
 
     app.run();
--- src/core/window.rs
+++ src/core/window.rs
@@ -1,28 +1,30 @@
 use crate::engine::opengl::{gl_clear_color, gl_viewport};
-use crate::windowing::glfw::{glfw_create_window, glfw_poll_events, glfw_set_window_user_pointer, glfw_swap_buffers, glfw_window_should_close, GLFWwindow};
+use crate::windowing::glfw::{glfw_create_window, glfw_get_window_user_pointer, glfw_poll_events, glfw_set_window_user_pointer, glfw_swap_buffers, glfw_window_should_close, GLFWwindow};
 use std::ffi::c_void;
 
 pub struct Window{
+    width: i32,
+    height: i32,
     glfw_window: *const GLFWwindow,
+    on_resize: Option<Box<dyn FnMut(i32, i32)>>,
 }
 
 extern "C" fn on_viewport_resized(_window: *const GLFWwindow, width: i32, height: i32) {
     gl_viewport(0, 0, width, height);
-    /*
     let user_ptr = glfw_get_window_user_pointer(_window);
     if !user_ptr.is_null() {
         unsafe {
             let window_ref: &mut Window = &mut *(user_ptr as *mut Window);
-            //window_ref.handle_resize(width, height);
+            window_ref._on_resize(width, height);
         }
-    }*/
+    }
 }
 
 
 impl Window{
     pub fn new(title: &str, width: i32, height: i32)->Box<Self>{
         let glfw_window = glfw_create_window(title, width, height, Some(on_viewport_resized));
-        let mut window = Box::new(Window{glfw_window});
+        let mut window = Box::new(Window{glfw_window, width, height,  on_resize: None});
         glfw_set_window_user_pointer(glfw_window, &mut *window as *mut _ as *mut c_void);
         window
     }
@@ -38,5 +40,18 @@ impl Window{
     pub fn poll_events(&self) {
         glfw_poll_events();
     }
-    
+
+    pub fn set_on_resize<F>(&mut self, f: F)
+    where
+        F: FnMut(i32, i32) + 'static,
+    {
+        self.on_resize = Some(Box::new(f));
+    }
+    pub fn _on_resize(&mut self, width: i32, height: i32) {
+        self.width = width;
+        self.height = height;
+        if let Some(callback) = &mut self.on_resize {
+            callback(width, height);
+        }
+    }
 }
